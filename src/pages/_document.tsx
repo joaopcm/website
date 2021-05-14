a4c0bcd862affe9244fea618328ebc032bdc7417
Joao Melo <jopcmelo@gmail.com>
2021-05-14 15:36:20 -0300

    feat: add external font family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,8 +16,8 @@ export const theme = extendTheme({
     },
   },
   fonts: {
-    heading: "Roboto",
-    body: "Roboto",
+    heading: "Poppins",
+    body: "Poppins",
   },
   styles: {
     global: {
