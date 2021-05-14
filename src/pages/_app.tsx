2b471b11fb5de20d9ee2e9af422278b028dd4d68
Joao Melo <jopcmelo@gmail.com>
2021-05-14 15:33:50 -0300

    feat: add chakra-ui integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
