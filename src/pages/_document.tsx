8b7ba7c9101e7ed235ef6d6ee30b653776cd3a37
Joao Melo <jopcmelo@gmail.com>
2021-05-15 16:56:44 -0300

    feat: add a favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/images/favicon.ico" />
         </Head>
         <body>
           <Main />
