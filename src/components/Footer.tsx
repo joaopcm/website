12da37405979ca2cea4d0a127411e4fde81538a8
Joao Melo <jopcmelo@gmail.com>
2021-06-29 21:08:12 -0300

    fix: fix logo color on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,19 +3,13 @@ import { Logo } from "./Header/Logo";
 
 export function Footer() {
   return (
-    <Box
-      bgColor="black"
-      px={["2rem", "6rem"]}
-      py="2rem"
-      color="gray.300"
-      fontSize="sm"
-    >
+    <Box bgColor="black" px={["2rem", "6rem"]} py="2rem" fontSize="sm">
       <SimpleGrid columns={[1, 2]} spacing={10}>
         <Box display={["none", "block"]}>
           <Logo />
         </Box>
 
-        <Flex align="center" justify={["center", "flex-end"]}>
+        <Flex align="center" justify={["center", "flex-end"]} color="gray.300">
           <Text>
             Copyrights ® {new Date().getFullYear()} jopcmelo. All rights
             reserved.
