3a5a0513b98100c22279f17662ee20e114c55562
João Melo <joao.melo@coinbase.com>
2022-07-01 21:37:59 -0300

    fix: fix header alignment

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from "../Link";
 
 export function Logo() {
   return (
-    <Link _hover={{ textDecoration: 'none' }} href="/" >
+    <Link _hover={{ textDecoration: 'none' }} href="/">
       <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight">
         João Melo
         <Text color="brand.cyan.500" as="span" ml="1">
